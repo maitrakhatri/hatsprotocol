673f85337975496f0c12a170b33270c767584fae
Maitra Khatri <maitrakhatri@192.168.1.5>
2023-12-18 13:26:49 +0530

    demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { useAccount, useNetwork, useWalletClient } from "wagmi";
 import { getPublicClient } from "@wagmi/core";
 import { HatsClient } from "@hatsprotocol/sdk-v1-core";
+import { useEffect, useState } from "react";
 
 function App() {
   const { chain } = useNetwork();
   const publicClient = getPublicClient({ chainId: 5 });
   const { data: walletClient } = useWalletClient();
   const { address } = useAccount();
+  const [userAddress, setUserAddress] = useState(address);
+  const [isProductDesigner, setIsProductDesigner] = useState<boolean>(false);
+  const [isCusSup, setIsCusSup] = useState<boolean>(false);
+
+  const pdhatId = BigInt(
+    "161760091391597659884661404783576847334135720567102765703756909641728"
+  );
+
+  const cshatId = BigInt(
+    "161760091391597468324180298184800455458961011611171096381022569234432"
+  );
 
   const hatsClient = new HatsClient({
     chainId: chain?.id || 5,
@@ -17,45 +27,66 @@ function App() {
     walletClient: walletClient!,
   });
 
-  const hatsProtocol = async () => {
-    const hatId = BigInt(
-      "161760091391597659884661404783576847334135720567102765703756909641728"
-    );
-    const hat = await hatsClient.viewHat(hatId);
-    const isActive = await hatsClient.isActive(hatId);
-    const isEligible = await hatsClient.isEligible({
-      wearer: address!,
-      hatId,
-    });
+  useEffect(() => {
+    setIsProductDesigner(false);
+    setIsCusSup(false);
+    hatsProtocol();
+  }, [userAddress]);
 
-    const isWearer = await hatsClient.isWearerOfHat({
-      wearer: address!,
-      hatId,
+  const hatsProtocol = async () => {
+    const isWearerofPD = await hatsClient.isWearerOfHat({
+      wearer: userAddress!,
+      hatId: pdhatId,
     });
-    console.log({ hat, isActive, isEligible, isWearer });
+    if (isWearerofPD) setIsProductDesigner(true);
+
+    const isWearerofCS = await hatsClient.isWearerOfHat({
+      wearer: userAddress!,
+      hatId: cshatId,
+    });
+    if (isWearerofCS) setIsCusSup(true);
   };
 
   return (
-    <>
+    <div className="App">
+      <button
+        onClick={() =>
+          setUserAddress("0x3216187edfd4a808f95f23702ec971e301706f2b")
+        }
+      >
+        <h3>Switch to Product Designer</h3>
+      </button>
+      <button
+        onClick={() =>
+          setUserAddress("0x516cafd745ec780d20f61c0d71fe258ea765222d")
+        }
+      >
+        <h3>Switch to Customer Support</h3>
+      </button>
+      <button onClick={() => setUserAddress(address)}>
+        <h3>Switch to Connected Wallet</h3>
+      </button>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h2>Product Designer only content</h2>
+        <div
+          style={{
+            backgroundColor: isProductDesigner ? "green" : "red",
+          }}
+        >
+          <h2>{isProductDesigner ? "Access granted" : "Access denied"}</h2>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => hatsProtocol()}>count is </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div>
+        <h2>Customer Support only content</h2>
+        <div
+          style={{
+            backgroundColor: isCusSup ? "green" : "red",
+          }}
+        >
+          <h2>{isCusSup ? "Access granted" : "Access denied"}</h2>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
 }
 
